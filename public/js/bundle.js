fbfdd0276faebc7bbbc591a57048161f25363015
Victor <victorsetlhare@gmail.com>
2020-05-12 21:32:11 +0100

    add currencies

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -150,7 +150,7 @@ eval("var g;\n\n// This works in non-strict mode\ng = (function() {\n\treturn th
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ \"./node_modules/vue/dist/vue.esm.js\");\n\n\ndocument.addEventListener('DOMContentLoaded', () => {\n    new vue__WEBPACK_IMPORTED_MODULE_0__[\"default\"]({\n        el: \"#app\",\n        \n    })\n})\n\n//# sourceURL=webpack:///./src/app.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ \"./node_modules/vue/dist/vue.esm.js\");\n\n\ndocument.addEventListener('DOMContentLoaded', () => {\n    new vue__WEBPACK_IMPORTED_MODULE_0__[\"default\"]({\n        el: \"#app\",\n        data: {\n            currencies: []\n        },\n        mounted(){\n            this.getCurrencies()\n        },\n        methods: {\n            getCuurrencies: function(){\n                fetch(\"https://api.exchangeratesapi.io/latest\")\n                .then(resolve => resolve.json())\n                .then(currencies => this.currencies = currencies)\n            }\n        }\n\n\n    })\n})\n\n//# sourceURL=webpack:///./src/app.js?");
 
 /***/ })
 
--- src/app.js
+++ src/app.js
@@ -3,6 +3,20 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         el: "#app",
-        
+        data: {
+            currencies: []
+        },
+        mounted(){
+            this.getCurrencies()
+        },
+        methods: {
+            getCuurrencies: function(){
+                fetch("https://api.exchangeratesapi.io/latest")
+                .then(resolve => resolve.json())
+                .then(currencies => this.currencies = currencies)
+            }
+        }
+
+
     })
 })
